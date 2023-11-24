94299ea88ee784b41f726b8c2a8aa13b7b3e03d7
David4527 <6226409@edu.vaniercollege.qc.ca>
2023-11-23 19:33:12 -0500

    modified:   src/main/java/org/example/SchoolManagementSystem.java

--- src/main/java/org/example/SchoolManagementSystem.java
+++ src/main/java/org/example/SchoolManagementSystem.java
@@ -1,34 +1,41 @@
 package org.example;
 
 public class SchoolManagementSystem {
+
     public Department findDepartment(String departmentId) {
     return null;
     }
-    public static void printTeachers() {
+    public void printTeachers() {
 
     }
-    public static void modifyCourseTeacher(String Course, String Teacher) {
+    public void modifyCourseTeacher(String Course, String Teacher) {
 
     }
-    public static void addDepartment(String departmentName) {
+    public void addDepartment(String departmentName) {
+
+    }
+    public void printStudents() {
 
     }
     public Student findStudent(String studentId) {
         return null;
     }
-    public static void addCourse(String courseName,double credit,String id ) {
+    public void addCourse(String courseName,double credit,String id ) {
 
     }
-    public static void registerCourse(String Course,String Course1) {
+    public  void registerCourse(String Course,String Course1) {
 
     }
-    public static void addTeacher(String fname,String lname,String id) {
+    public void addTeacher(String fname,String lname,String id) {
 
     }
     public Course findCourse(String courseId) {
         return null;
     }
-    public static void addStudent(String fname, String lname, String id) {
+    public void printDepartments() {
+
+    }
+    public void addStudent(String fname, String lname, String id) {
 
     }
     public Teacher findTeacher(String teacherId) {
